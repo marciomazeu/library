25ddce30acf576753933c2753c5318b64790b752
marciomazeu <mazeu911@gmail.com>
2025-10-06 21:51:01 -0400

    update

---
+++ Library/Models/BorrowingRecords.cs
@@ -0,0 +1,15 @@
+﻿namespace Library.Models
+{
+    public class BorrowingRecords
+    {
+
+        public int BookId { get; set; }
+        public string BookName { get; set; } = string.Empty;
+        public string BookTitle { get; set; } 
+        public string BookDescription { get; set; } = string.Empty;
+        public int PersonId { get; set; }
+        public string PersonName { get; set; }
+        public DateTime BorrowDate { get; set; } = DateTime.Now;
+        public DateTime BorrowDateDelivery { get; set; } = DateTime.Now;
+    }
+}
